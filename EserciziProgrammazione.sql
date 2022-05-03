58fa665a7449e88365e74d210e1606a92c13b362
Davidebgd <davide.balto@gmail.com>
2022-05-03 13:22:25 +0200

    Correz es 4

--- EserciziProgrammazione.sql
+++ EserciziProgrammazione.sql
@@ -18,7 +18,7 @@ from Programmatore,Programma,Autore
 where Programmatore.codice=Autore.codice and Autore.id=Programma.id and Programma.linguaggio != "java" and Programmatore.categoria=10
 
 
-select P1.codice, p2.codice,
-from Programmatore p1, Programmatore p2, Programma,Autore
-where  P1.codice=Autore.codice and Autore.id=Programma.id and Programma.linguaggio = "Python" 
-    and  P2.codice=Autore.codice 
+select distinct a1.codice as programma1 , a2.codice as programma2
+from Programma join autore a1 on Programma.id=a1.id 
+     join autore a2 on Programma.id=a2.id 
+where  Programma.linguaggio = "Python" and a1.codice!=a2.codice
