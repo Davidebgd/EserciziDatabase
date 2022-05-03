c34ebc7ff30012a0ae5f1f5132c70b2964613f4c
Davidebgd <davide.balto@gmail.com>
2022-05-03 12:41:28 +0200

    Es 4

--- EserciziProgrammazione.sql
+++ EserciziProgrammazione.sql
@@ -16,3 +16,9 @@ order by Programmatore.Nome
 select Programmatore.codice, Programma.anno
 from Programmatore,Programma,Autore
 where Programmatore.codice=Autore.codice and Autore.id=Programma.id and Programma.linguaggio != "java" and Programmatore.categoria=10
+
+
+select P1.codice, p2.codice,
+from Programmatore p1, Programmatore p2, Programma,Autore
+where  P1.codice=Autore.codice and Autore.id=Programma.id and Programma.linguaggio = "Python" 
+    and  P2.codice=Autore.codice 
