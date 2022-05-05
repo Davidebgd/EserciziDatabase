375d545da3eccd6cd81a073504a1b2d4c10f0cc3
Davidebgd <davide.balto@gmail.com>
2022-05-05 11:06:57 +0200

    ES 7 e 8

--- EserciziScalata.sql
+++ EserciziScalata.sql
@@ -33,3 +33,19 @@ FROM  Scalatore join Nazione on Scalatore.nazioneNascita = Nazione.nome
     join Scalata on Scalatore.cf = Scalata.scalatore
 WHERE Scalatore.nazioneNascita = Nazione.nome and Nazione.continente !="America"
 group by Scalatore.cf
+
+/*     ES 7     */
+SELECT  Nazione.nome, Scalata.anno, count(*)
+FROM Scalata join Nazione on Scalata.nazione = Nazione.nome
+group by Nazione.continente, Scalata.nazione,Scalata.anno
+having count(*)>1
+order by Nazione.continente, Scalata.anno
+
+
+/*     ES 8     */
+SELECT distinct Nazione.nome, count(Scalata.scalatore)/count(distinct Scalata.anno) as Media, Scalata.anno
+FROM Scalata join Nazione on Scalata.nazione = Nazione.nome
+    join Scalatore on Scalata.scalatore = Scalatore.cf
+WHERE Scalatore.nazioneNascita != Scalata.nazione
+group by Scalata.anno, Nazione.nome
+ORDER BY Nazione.nome
